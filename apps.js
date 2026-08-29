165743ce6cd344b41dbe63853db5d08117bd69e3
only-phoenix <sainiamit6838@gmail.com>
2026-08-29 21:00:22 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -231,3 +231,15 @@ function displayTimetable() {
 }
 
 displayTimetable();
+// ===== PAGE NAVIGATION =====
+
+function showPage(pageId) {
+
+    const pages = document.querySelectorAll(".page");
+
+    pages.forEach(page => {
+        page.classList.add("hidden");
+    });
+
+    document.getElementById(pageId).classList.remove("hidden");
+}
